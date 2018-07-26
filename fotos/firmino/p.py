7ac1cf42e9e75c8cdb9f466ee8e6b7b1aff8f7da
Ubuntu <ubuntu@ip-172-31-14-102.sa-east-1.compute.internal>
2018-07-26 15:57:04 +0000

    removendo as fotos do trabalho de ds

--- fotos/firmino/p.py
+++
@@ -1,9 +0,0 @@
-import os
-
-path =  os.getcwd()
-filenames = os.listdir(path)
-
-for filename in filenames:
-    print filename
-    os.rename(filename, filename.replace("firmino figurinhas", "firmino").lower())
-
